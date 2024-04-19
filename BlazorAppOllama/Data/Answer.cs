fe320edf2e6c9897316925d212b499d1de13c8d7
luis <luis@ROBTECH>
2024-04-18 19:04:42 -0500

    Add project files.

---
+++ BlazorAppOllama/Data/Answer.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorAppOllama.Data
+{
+    public class Answer
+    {
+        public string model { get; set; }
+        public string created_at { get; set; }
+        public string response { get; set; }
+        public bool done { get; set; }
+    }
+}
---
+++ BlazorAppOllama/Data/Question.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAppOllama.Data
+{
+    public class Question
+    {
+        public string model { get; set; } = "llama3";
+        public string prompt { get; set; } = string.Empty;
+        public bool stream { get; set; } = false;
+    }
+}
